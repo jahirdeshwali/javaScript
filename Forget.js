c2a7a77460cc7dcb0edc077d367e928b5700ea97
jahir khan <139323281+jahirdeshwali@users.noreply.github.com>
2023-07-13 10:29:19 +0530

    Add files via upload

---
+++ Forget.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+// import Toast from 'react-native-simple-toast';
+
+
+
+
+
+const Forget = () => {
+  return (
+    <View
+      style={{
+        flex: 1,backgroundColor:'white'}}>
+            <ActivityIndicator size="large" color="green" style={{marginTop:40}} />
+<View>
+
+  <Text style={{fontSize:30,textAlign:'center',color:'blue'}}>
+    Forget Password?
+    
+  </Text>
+  <Text style={{}}>
+      Forget Your Password
+    </Text>
+</View>
+
+
+
+
+    
+    </View>
+  );
+};
+
+export default Forget;
